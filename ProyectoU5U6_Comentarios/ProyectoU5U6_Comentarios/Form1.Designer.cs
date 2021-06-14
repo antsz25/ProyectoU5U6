2f86fb296d02990a1d1f37e8f8510229c43c5028
Jesus Antonio Sanchez Santos <80359412+antsz25@users.noreply.github.com>
2021-06-13 22:28:03 -0700

    V2
    Agregar comentario y recargar la forma hecha. Definidas las clases Comentario y comentariosDB. Definidas las funciones de busqueda en archivo, guardado en archivo, asi como la creación de este. Añadido la captura de ciertos errores, aunque aun no esta completo

--- ProyectoU5U6_Comentarios/ProyectoU5U6_Comentarios/Form1.Designer.cs
+++ ProyectoU5U6_Comentarios/ProyectoU5U6_Comentarios/Form1.Designer.cs
@@ -29,41 +29,21 @@ namespace ProyectoU5U6_Comentarios
         /// </summary>
         private void InitializeComponent()
         {
-            this.lbautor1 = new System.Windows.Forms.Label();
             this.LayoutComent = new System.Windows.Forms.TableLayoutPanel();
-            this.pComment1 = new System.Windows.Forms.Panel();
-            this.bt_like1 = new System.Windows.Forms.Button();
-            this.bt_resp1 = new System.Windows.Forms.Button();
-            this.lblikes1 = new System.Windows.Forms.Label();
-            this.rtxtb_comment1 = new System.Windows.Forms.RichTextBox();
-            this.lbfecha1 = new System.Windows.Forms.Label();
-            this.lbinaprop1 = new System.Windows.Forms.Label();
-            this.lbpubli = new System.Windows.Forms.Label();
-            this.rtxtb_publi = new System.Windows.Forms.RichTextBox();
-            this.btpubli = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.btpubli = new System.Windows.Forms.Button();
+            this.rtxtb_publi = new System.Windows.Forms.RichTextBox();
+            this.lbpubli = new System.Windows.Forms.Label();
             this.LayoutComent.SuspendLayout();
-            this.pComment1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // lbautor1
-            // 
-            this.lbautor1.AutoSize = true;
-            this.lbautor1.BackColor = System.Drawing.Color.Transparent;
-            this.lbautor1.Location = new System.Drawing.Point(4, 2);
-            this.lbautor1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lbautor1.Name = "lbautor1";
-            this.lbautor1.Size = new System.Drawing.Size(40, 20);
-            this.lbautor1.TabIndex = 3;
-            this.lbautor1.Text = "Autor";
-            // 
             // LayoutComent
             // 
-            this.LayoutComent.ColumnCount = 2;
+            this.LayoutComent.AutoScroll = true;
+            this.LayoutComent.ColumnCount = 1;
             this.LayoutComent.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.LayoutComent.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.LayoutComent.Controls.Add(this.pComment1, 0, 0);
+            this.LayoutComent.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.LayoutComent.Controls.Add(this.panel2, 0, 1);
             this.LayoutComent.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.LayoutComent.Location = new System.Drawing.Point(0, 12);
@@ -76,107 +56,17 @@ namespace ProyectoU5U6_Comentarios
             this.LayoutComent.TabIndex = 0;
             this.LayoutComent.Paint += new System.Windows.Forms.PaintEventHandler(this.LayoutComent_Paint);
             // 
-            // pComment1
+            // panel2
             // 
-            this.pComment1.AutoScroll = true;
-            this.pComment1.Controls.Add(this.lbinaprop1);
-            this.pComment1.Controls.Add(this.lbfecha1);
-            this.pComment1.Controls.Add(this.lbautor1);
-            this.pComment1.Controls.Add(this.rtxtb_comment1);
-            this.pComment1.Controls.Add(this.bt_resp1);
-            this.pComment1.Controls.Add(this.lblikes1);
-            this.pComment1.Controls.Add(this.bt_like1);
-            this.pComment1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pComment1.Location = new System.Drawing.Point(2, 3);
-            this.pComment1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
-            this.pComment1.Name = "pComment1";
-            this.pComment1.Size = new System.Drawing.Size(917, 104);
-            this.pComment1.TabIndex = 6;
-            // 
-            // bt_like1
-            // 
-            this.bt_like1.Location = new System.Drawing.Point(8, 69);
-            this.bt_like1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
-            this.bt_like1.Name = "bt_like1";
-            this.bt_like1.Size = new System.Drawing.Size(81, 28);
-            this.bt_like1.TabIndex = 0;
-            this.bt_like1.Text = "Me gusta";
-            this.bt_like1.UseVisualStyleBackColor = true;
-            this.bt_like1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // bt_resp1
-            // 
-            this.bt_resp1.Location = new System.Drawing.Point(97, 69);
-            this.bt_resp1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
-            this.bt_resp1.Name = "bt_resp1";
-            this.bt_resp1.Size = new System.Drawing.Size(87, 28);
-            this.bt_resp1.TabIndex = 1;
-            this.bt_resp1.Text = "Responder";
-            this.bt_resp1.UseVisualStyleBackColor = true;
-            this.bt_resp1.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // lblikes1
-            // 
-            this.lblikes1.AutoSize = true;
-            this.lblikes1.BackColor = System.Drawing.Color.Transparent;
-            this.lblikes1.Location = new System.Drawing.Point(198, 74);
-            this.lblikes1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lblikes1.Name = "lblikes1";
-            this.lblikes1.Size = new System.Drawing.Size(41, 20);
-            this.lblikes1.TabIndex = 5;
-            this.lblikes1.Text = "Likes";
-            this.lblikes1.Click += new System.EventHandler(this.lblikes1_Click);
-            // 
-            // rtxtb_comment1
-            // 
-            this.rtxtb_comment1.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.rtxtb_comment1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.rtxtb_comment1.Location = new System.Drawing.Point(8, 25);
-            this.rtxtb_comment1.Name = "rtxtb_comment1";
-            this.rtxtb_comment1.ReadOnly = true;
-            this.rtxtb_comment1.Size = new System.Drawing.Size(905, 41);
-            this.rtxtb_comment1.TabIndex = 2;
-            this.rtxtb_comment1.Text = "fffffffff\nfffff\nfff";
-            this.rtxtb_comment1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
-            // 
-            // lbfecha1
-            // 
-            this.lbfecha1.AutoSize = true;
-            this.lbfecha1.BackColor = System.Drawing.Color.Transparent;
-            this.lbfecha1.Location = new System.Drawing.Point(649, 2);
-            this.lbfecha1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lbfecha1.Name = "lbfecha1";
-            this.lbfecha1.Size = new System.Drawing.Size(46, 20);
-            this.lbfecha1.TabIndex = 6;
-            this.lbfecha1.Text = "Fecha";
-            // 
-            // lbinaprop1
-            // 
-            this.lbinaprop1.AutoSize = true;
-            this.lbinaprop1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbinaprop1.ForeColor = System.Drawing.Color.Blue;
-            this.lbinaprop1.Location = new System.Drawing.Point(760, 74);
-            this.lbinaprop1.Name = "lbinaprop1";
-            this.lbinaprop1.Size = new System.Drawing.Size(153, 15);
-            this.lbinaprop1.TabIndex = 7;
-            this.lbinaprop1.Text = "Marcar como inapropiado";
-            // 
-            // lbpubli
-            // 
-            this.lbpubli.AutoSize = true;
-            this.lbpubli.Location = new System.Drawing.Point(3, 0);
-            this.lbpubli.Name = "lbpubli";
-            this.lbpubli.Size = new System.Drawing.Size(93, 20);
-            this.lbpubli.TabIndex = 0;
-            this.lbpubli.Text = "Nombre_publi";
-            // 
-            // rtxtb_publi
-            // 
-            this.rtxtb_publi.Location = new System.Drawing.Point(7, 23);
-            this.rtxtb_publi.Name = "rtxtb_publi";
-            this.rtxtb_publi.Size = new System.Drawing.Size(905, 91);
-            this.rtxtb_publi.TabIndex = 1;
-            this.rtxtb_publi.Text = "";
+            this.panel2.Controls.Add(this.btpubli);
+            this.panel2.Controls.Add(this.rtxtb_publi);
+            this.panel2.Controls.Add(this.lbpubli);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(3, 185);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1075, 162);
+            this.panel2.TabIndex = 7;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
             // btpubli
             // 
@@ -186,18 +76,24 @@ namespace ProyectoU5U6_Comentarios
             this.btpubli.TabIndex = 2;
             this.btpubli.Text = "Publicar";
             this.btpubli.UseVisualStyleBackColor = true;
+            this.btpubli.Click += new System.EventHandler(this.btpubli_Click);
             // 
-            // panel2
+            // rtxtb_publi
             // 
-            this.panel2.Controls.Add(this.btpubli);
-            this.panel2.Controls.Add(this.rtxtb_publi);
-            this.panel2.Controls.Add(this.lbpubli);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(3, 113);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(915, 155);
-            this.panel2.TabIndex = 7;
-            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            this.rtxtb_publi.Location = new System.Drawing.Point(7, 23);
+            this.rtxtb_publi.Name = "rtxtb_publi";
+            this.rtxtb_publi.Size = new System.Drawing.Size(905, 91);
+            this.rtxtb_publi.TabIndex = 1;
+            this.rtxtb_publi.Text = "";
+            // 
+            // lbpubli
+            // 
+            this.lbpubli.AutoSize = true;
+            this.lbpubli.Location = new System.Drawing.Point(3, 0);
+            this.lbpubli.Name = "lbpubli";
+            this.lbpubli.Size = new System.Drawing.Size(93, 20);
+            this.lbpubli.TabIndex = 0;
+            this.lbpubli.Text = "Nombre_publi";
             // 
             // Form1
             // 
@@ -216,8 +112,6 @@ namespace ProyectoU5U6_Comentarios
             this.Text = "Comentarios";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.LayoutComent.ResumeLayout(false);
-            this.pComment1.ResumeLayout(false);
-            this.pComment1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.ResumeLayout(false);
@@ -225,15 +119,7 @@ namespace ProyectoU5U6_Comentarios
         }
 
         #endregion
-        private System.Windows.Forms.Label lbautor1;
         private System.Windows.Forms.TableLayoutPanel LayoutComent;
-        private System.Windows.Forms.Panel pComment1;
-        private System.Windows.Forms.Button bt_resp1;
-        private System.Windows.Forms.Button bt_like1;
-        private System.Windows.Forms.Label lblikes1;
-        private System.Windows.Forms.RichTextBox rtxtb_comment1;
-        private System.Windows.Forms.Label lbfecha1;
-        private System.Windows.Forms.Label lbinaprop1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button btpubli;
         private System.Windows.Forms.RichTextBox rtxtb_publi;
--- ProyectoU5U6_Comentarios/ProyectoU5U6_Comentarios/Form1.cs
+++ ProyectoU5U6_Comentarios/ProyectoU5U6_Comentarios/Form1.cs
@@ -11,51 +11,7 @@ using System.Windows.Forms;
 
 namespace ProyectoU5U6_Comentarios
 {
-    class Comentario
-    {
-        public int id { get; set; }
-        public string autor { get; set; }
-        public string fecha_publi { get; set; }
-        public string comentario { get; set; }
-        public string dir_ip { get; set; }
-        public int likes { get; set; }
-        public int inapropiado { get; set; }
 
-       
-        public override string ToString()
-        {
-            return string.Format($"{id},{autor},{fecha_publi},{comentario},{dir_ip},{inapropiado},{likes}");
-        }
-       
-    }
-    class ComentariosDB
-    {
-        public static List<Comentario> ReadFromFile(string path)
-        {
-            List<Comentario> products = new List<Comentario>();
-
-            StreamReader textIn = new StreamReader(new FileStream(path, FileMode.Open, FileAccess.Read));
-
-            while (textIn.Peek() != -1)    // Leer hasta el final
-            {
-                string row = textIn.ReadLine();  // row =  "1000|Prueba|13/Junio/2021|198.192.0.1|Esto es una prueba|3|0"
-                string[] columns = row.Split('|'); // ["1000","Prueba","13/Junio/2021","Esto es una prueba","3","0"]; 
-                Comentario c = new Comentario();
-                c.id = int.Parse(columns[0]);
-                c.autor = columns[1];
-                c.fecha_publi = columns[2];
-                c.dir_ip = columns[3];
-                c.comentario = columns[4];
-                c.likes = int.Parse(columns[5]);
-                c.inapropiado = int.Parse(columns[6]);
-                products.Add(c);
-            }
-
-            textIn.Close();
-
-            return products;
-        }
-    }
     public partial class Form1 : Form
     {
         private Label lbinapropi;
@@ -66,20 +22,52 @@ namespace ProyectoU5U6_Comentarios
         private Button bt_respi;
         private Label lblikesi;
         private Button bt_likei;
-        private int i = 0;
+
         public Form1()
         {
             InitializeComponent();
+
+            Reescribe();
+            AgregarComentarios(Reescribe());
+        }
+        private List<Comentario> Reescribe()
+        {
             List<Comentario> comments = ComentariosDB.ReadFromFile(Application.StartupPath + @"/DB/Comentarios.txt");
+            List<Comentario> inapropiate = ComentariosDB.ReadFromFile(Application.StartupPath + @"/DB/ComentariosInapropiados.txt");
+            for (int i = 0; i <= comments.Count - 1; i++)
+            {
+                for (int j = 0; j <= inapropiate.Count - 1; j++)
+                {
+                    comments.RemoveAll(x => x.id == inapropiate[j].id);
+                }
+            }
+            comments.Sort();
+            comments.Reverse();
+            return comments;
+        }
+        private void AgregarComentarios(List<Comentario> comments)
+        {
             foreach (var comment in comments)
             {
+                // 
+                // lbautori
+                // 
+                lbautori = new Label();
+                this.lbautori.AutoSize = true;
+                this.lbautori.BackColor = System.Drawing.Color.Transparent;
+                this.lbautori.Location = new System.Drawing.Point(4, 2);
+                this.lbautori.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+                this.lbautori.Name = "lbautor" + comment.id;
+                this.lbautori.Size = new System.Drawing.Size(40, 20);
+                this.lbautori.TabIndex = 3;
+                this.lbautori.Text = comment.autor;
                 // 
                 // bt_likei
                 // 
                 bt_likei = new Button();
                 this.bt_likei.Location = new System.Drawing.Point(8, 69);
                 this.bt_likei.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
-                this.bt_likei.Name = "bt_like"+comment.id;
+                this.bt_likei.Name = "bt_like" + comment.id;
                 this.bt_likei.Size = new System.Drawing.Size(81, 28);
                 this.bt_likei.TabIndex = 0;
                 this.bt_likei.Text = "Me gusta";
@@ -91,7 +79,7 @@ namespace ProyectoU5U6_Comentarios
                 bt_respi = new Button();
                 this.bt_respi.Location = new System.Drawing.Point(97, 69);
                 this.bt_respi.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
-                this.bt_respi.Name = "bt_resp"+comment.id;
+                this.bt_respi.Name = "bt_resp" + comment.id;
                 this.bt_respi.Size = new System.Drawing.Size(87, 28);
                 this.bt_respi.TabIndex = 1;
                 this.bt_respi.Text = "Responder";
@@ -105,10 +93,10 @@ namespace ProyectoU5U6_Comentarios
                 this.lblikesi.BackColor = System.Drawing.Color.Transparent;
                 this.lblikesi.Location = new System.Drawing.Point(198, 74);
                 this.lblikesi.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-                this.lblikesi.Name = "lblikes"+comment.id;
+                this.lblikesi.Name = "lblikes" + comment.id;
                 this.lblikesi.Size = new System.Drawing.Size(41, 20);
                 this.lblikesi.TabIndex = 5;
-                this.lblikesi.Text = "Likes: "+comment.likes;
+                this.lblikesi.Text = "Likes: " + comment.likes;
                 // 
                 // rtxtb_commenti
                 // 
@@ -116,7 +104,7 @@ namespace ProyectoU5U6_Comentarios
                 this.rtxtb_commenti.BackColor = System.Drawing.SystemColors.ControlLightLight;
                 this.rtxtb_commenti.BorderStyle = System.Windows.Forms.BorderStyle.None;
                 this.rtxtb_commenti.Location = new System.Drawing.Point(8, 25);
-                this.rtxtb_commenti.Name = "rtxtb_comment"+comment.id;
+                this.rtxtb_commenti.Name = "rtxtb_comment" + comment.id;
                 this.rtxtb_commenti.ReadOnly = true;
                 this.rtxtb_commenti.Size = new System.Drawing.Size(905, 41);
                 this.rtxtb_commenti.TabIndex = 2;
@@ -130,10 +118,10 @@ namespace ProyectoU5U6_Comentarios
                 this.lbfechai.BackColor = System.Drawing.Color.Transparent;
                 this.lbfechai.Location = new System.Drawing.Point(649, 2);
                 this.lbfechai.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-                this.lbfechai.Name = "lbfecha"+comment.id;
+                this.lbfechai.Name = "lbfecha" + comment.id;
                 this.lbfechai.Size = new System.Drawing.Size(46, 20);
                 this.lbfechai.TabIndex = 6;
-                this.lbfechai.Text = "Fecha: "+comment.fecha_publi;
+                this.lbfechai.Text = "Fecha: " + comment.fecha_publi;
                 // 
                 // lbinapropi
                 // 
@@ -142,7 +130,7 @@ namespace ProyectoU5U6_Comentarios
                 this.lbinapropi.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
                 this.lbinapropi.ForeColor = System.Drawing.Color.Blue;
                 this.lbinapropi.Location = new System.Drawing.Point(760, 74);
-                this.lbinapropi.Name = "lbinaprop"+comment.id;
+                this.lbinapropi.Name = "lbinaprop" + comment.id;
                 this.lbinapropi.Size = new System.Drawing.Size(153, 15);
                 this.lbinapropi.TabIndex = 7;
                 this.lbinapropi.Text = "Marcar como inapropiado";
@@ -161,13 +149,177 @@ namespace ProyectoU5U6_Comentarios
                 this.pComment2.Dock = System.Windows.Forms.DockStyle.Top;
                 this.pComment2.Location = new System.Drawing.Point(2, 3);
                 this.pComment2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
-                this.pComment2.Name = "pComment" + i;
+                this.pComment2.Name = "pComment" + comment.id;
                 this.pComment2.Size = new System.Drawing.Size(917, 104);
                 this.pComment2.TabIndex = 6;
-                LayoutComent.Controls.Add(pComment2,0,0);
+                LayoutComent.Controls.Add(pComment2, 0, 0);
             }
         }
+        class Comentario : IComparable
+        {
+            public int id { get; set; }
+            public string autor { get; set; }
+            public string fecha_publi { get; set; }
+            public string comentario { get; set; }
+            public string dir_ip { get; set; }
+            public int likes { get; set; }
+            public int inapropiado { get; set; }
 
+            public int CompareTo(object obj)
+            {
+                return this.id.CompareTo((obj as Comentario).id);
+            }
+
+            public override string ToString()
+            {
+                return string.Format($"{id},{autor},{fecha_publi},{comentario},{dir_ip},{inapropiado},{likes}");
+            }
+            public Comentario()
+            {
+
+            }
+
+            public Comentario(int id, string autor, string fecha_publi, string comentario, string dir_ip, int likes, int inapropiado)
+            {
+                this.id = id;
+                this.autor = autor;
+                this.fecha_publi = fecha_publi;
+                this.comentario = comentario;
+                this.dir_ip = dir_ip;
+                this.likes = likes;
+                this.inapropiado = inapropiado;
+            }
+        }
+        class ComentariosDB
+        {
+            public static void SaveToFile(Comentario comment, string path)
+            {
+                StreamWriter textOut = null;
+
+                try
+                {
+                    if (File.Exists(path)) {
+                        List<Comentario> comments = ComentariosDB.ReadFromFile(Application.StartupPath + @"/DB/Comentarios.txt");
+                        textOut = new StreamWriter(new FileStream(path, FileMode.Open, FileAccess.Write));
+                        // row =  "1000|Prueba|13/Junio/2021|198.192.0.1|Esto es una prueba|3|0"
+                        foreach(var c in comments)
+                        {
+                            textOut.Write(c.id + "|");
+                            textOut.Write(c.autor + "|");
+                            textOut.Write(c.fecha_publi + "|");
+                            textOut.Write(c.dir_ip + "|");
+                            textOut.Write(c.comentario + "|");
+                            textOut.Write(c.likes + "|");
+                            textOut.WriteLine(c.inapropiado);
+                        }
+                        textOut.Write(comment.id + "|");
+                        textOut.Write(comment.autor + "|");
+                        textOut.Write(comment.fecha_publi + "|");
+                        textOut.Write(comment.dir_ip + "|");
+                        textOut.Write(comment.comentario + "|");
+                        textOut.Write(comment.likes + "|");
+                        textOut.WriteLine(comment.inapropiado);
+                        textOut.Close();
+
+                    }
+                    else
+                    {
+                        textOut = new StreamWriter(new FileStream(path, FileMode.Create, FileAccess.Write));
+                        // row =  "1000|Prueba|13/Junio/2021|198.192.0.1|Esto es una prueba|3|0"
+                        textOut.Write(comment.id + "|");
+                        textOut.Write(comment.autor + "|");
+                        textOut.Write(comment.fecha_publi + "|");
+                        textOut.Write(comment.dir_ip + "|");
+                        textOut.Write(comment.comentario + "|");
+                        textOut.Write(comment.likes + "|");
+                        textOut.WriteLine(comment.inapropiado);
+                        textOut.Close();
+                    }
+
+
+                }
+
+                catch (IOException)
+                {
+                    Console.WriteLine("Ya existe el archivo");
+                }
+
+                catch (Exception)
+                {
+                    Console.WriteLine("Error");
+                }
+
+                finally
+                {
+                    if (textOut != null)
+                        textOut.Close();
+                }
+
+
+            }
+            public static List<Comentario> ReadFromFile(string path)
+            {
+                List<Comentario> comments = new List<Comentario>();
+                try
+                {
+                    if (File.Exists(path)) {
+                        StreamReader textIn = new StreamReader(new FileStream(path, FileMode.Open, FileAccess.Read));
+                        while (textIn.Peek() != -1)    // Leer hasta el final
+                        {
+                                string row = textIn.ReadLine();  // row =  "1000|Prueba|13/Junio/2021|198.192.0.1|Esto es una prueba|3|0"
+                                string[] columns = row.Split('|'); // ["1000","Prueba","13/Junio/2021","Esto es una prueba","3","0"]; 
+                                Comentario c = new Comentario();
+                                c.id = int.Parse(columns[0]);
+                                c.autor = columns[1];
+                                c.fecha_publi = columns[2];
+                                c.dir_ip = columns[3];
+                                c.comentario = columns[4];
+                                c.likes = int.Parse(columns[5]);
+                                c.inapropiado = int.Parse(columns[6]);
+                                comments.Add(c);
+                        }
+
+                        textIn.Close();
+
+                        return comments;
+                    }
+                    else
+                    {
+
+                        StreamReader textIn = new StreamReader(new FileStream(path, FileMode.Create));
+                        textIn.Close();
+
+                        return comments;
+                    }
+                }
+                catch(IOException e)
+                {
+                    MessageBox.Show(e.ToString());
+                }
+                catch(Exception e)
+                {
+                    MessageBox.Show(e.ToString());
+                }
+                return comments;
+            }
+            public static int GetLastID(string path)
+            {
+                try
+                {
+                    List<Comentario> comments = ReadFromFile(path);
+
+
+                    var filtro_productos = from c in comments
+                                           select c;
+                    return filtro_productos.Last().id;
+                }
+                catch(Exception e)
+                {
+                    MessageBox.Show(e.ToString());
+                    return 999;
+                }
+            }
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
 
@@ -201,6 +353,25 @@ namespace ProyectoU5U6_Comentarios
         private void panel2_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+
+        private void btpubli_Click(object sender, EventArgs e)
+        {
+            string date = DateTime.Today.ToShortDateString();
+            int last = ComentariosDB.GetLastID(Application.StartupPath + @"/DB/Comentarios.txt");
+            Comentario c = new Comentario(last + 1, lbpubli.Text,date, rtxtb_publi.Text, "198.192.0.0", 0, 0);
+            ComentariosDB.SaveToFile(c, Application.StartupPath + @"/DB/Comentarios.txt");
+            // List<Comentario> comments = ComentariosDB.ReadFromFile(Application.StartupPath + @"/DB/Comentarios.txt");
+
+
+            /*for (int i = 0; i <= LayoutComent.RowCount - 1; i++)
+            {
+                Control a = LayoutComent.GetControlFromPosition(0,i);
+                rtxtb_publi.Text += a.Name + "\n";
+            }*/
+            this.Controls.Clear();
+            this.InitializeComponent();
+            this.AgregarComentarios(Reescribe());
         }
     }
 }
